0ab75221296c25b83b5a62e58af4088acb8ae489
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-12-25 09:28:46 +0900

    remove: サンプルページ・サンプルコンポーネント

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-// サンプルページです！！色々変更して使ってください！！
-import { Stack, Typography } from "@mui/material";
-
-import MyComponent from "@/components/myComponent";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header></Header>
-      <Stack height="100lvh" justifyContent="center" alignItems="center" gap="32px">
-        <Typography id="login_heading" variant="h1" fontSize="1.5rem">ログインフォーム（サンプル）</Typography>
-        <MyComponent />
-      </Stack>
+
     </>
   );
 }
