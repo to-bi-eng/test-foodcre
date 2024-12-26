4010fd02702d90f36a82e2878dd0e341c6334d98
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-12-26 22:31:08 +0900

    update: フッターのスタイル

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import AppBar from '@mui/material/AppBar';
-import { Box } from "@mui/material";
+import { Box, Typography, Link as MuiLink, IconButton } from "@mui/material";
 import Grid from '@mui/material/Grid2';
+import Link from "next/link";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import styles from '@/styles/Footer.module.css'
 
 export default function Footer() {
@@ -9,7 +14,6 @@ export default function Footer() {
     <Box
       component="footer"
       sx={{
-        textAlign: "center",
         position: "fixed",
         inset: 0,
         top: 'auto',
@@ -17,17 +21,117 @@ export default function Footer() {
       }}
       className={styles.box}
     >
-      <AppBar sx={{
-        padding: "16px",
-      }}
+      <AppBar
+        sx={{
+          padding: "16px",
+        }}
+        position="sticky"
         color="primary"
         className={styles.footer}
       >
         <Grid container spacing={2} justifyContent="space-between" alignItems="center">
-          <Grid size={6}>
-
+          <Grid
+            size={6}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem'
+            }}>
+            <MuiLink component={Link} href="/contact" underline="none">
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{
+                  color: '#333333'
+                }}>
+                お問い合わせ
+              </Typography>
+            </MuiLink>
+            <MuiLink component={Link} href="https://www.hachiban.jp/" underline="none" target="blank">
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{
+                  color: '#333333'
+                }}>
+                ８番らーめん公式HP
+              </Typography>
+            </MuiLink>
+            <MuiLink component={Link} href="https://foodcreation.github.io/" underline="none" target="blank">
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{
+                  color: '#333333'
+                }}>
+                プロジェクト公式HP
+              </Typography>
+            </MuiLink>
           </Grid>
-          <Grid size={6}>
+          <Grid
+            size={6}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem'
+            }}>
+            <Box component='div'>
+              <Box
+                component='div'
+                sx={{
+                  display: 'flex',
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}>
+                <IconButton href="https://twitter.com/8ban_ramen" aria-label="Twitter" target="blank">
+                  <TwitterIcon />
+                </IconButton>
+                <IconButton href="https://www.instagram.com/8ban_ramen/" aria-label="Instagram" target="blank">
+                  <InstagramIcon />
+                </IconButton>
+                <IconButton href="https://www.facebook.com/hachibanramen.jp/" aria-label="Facebook" target="blank">
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton href="https://www.youtube.com/user/hachibanramen" aria-label="YouTube" target="blank">
+                  <YouTubeIcon />
+                </IconButton>
+              </Box>
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{
+                  color: '#333333',
+                  textAlign: 'center'
+                }}>
+                ８番らーめん公式SNS
+              </Typography>
+            </Box>
+            <Box component='div'>
+              <Box
+                component='div'
+                sx={{
+                  display: 'flex',
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}>
+                <IconButton href="https://twitter.com/foodc_8ban" aria-label="Twitter" target="blank">
+                  <TwitterIcon />
+                </IconButton>
+                <IconButton href="https://www.instagram.com/foodc_8ban/" aria-label="Instagram" target="blank">
+                  <InstagramIcon />
+                </IconButton>
+
+              </Box>
+              <Typography
+                variant="body1"
+                component="div"
+                sx={{
+                  color: '#333333',
+                  textAlign: 'center'
+                }}>
+                プロジェクト公式SNS
+              </Typography>
+            </Box>
           </Grid>
         </Grid>
       </AppBar>
