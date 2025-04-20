a104f663e211c4fc3aec5114a20fd080dcbe307b
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2025-04-20 20:51:33 +0900

    shemaの変更

--- init/schema.sql
+++ init/schema.sql
@@ -1,27 +1,16 @@
 -- メニューテーブル
 CREATE TABLE menus (
-    menu_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     menu_name VARCHAR(255) NOT NULL,
     menu_contact TEXT,
     point_cost INT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    is_enabled BOOLEAN DEFAULT TRUE
-);
-
--- クーポンテーブル
-CREATE TABLE coupons (
-    coupon_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    menu_id INT,
-    used_at DATETIME,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    date_of_expiry DATETIME,
-    FOREIGN KEY (menu_id) REFERENCES menu(menu_id)
+    is_enabled TINYINT(1) DEFAULT 1
 );
 
 -- ユーザーテーブル
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     reset_password VARCHAR(255),
@@ -29,26 +18,38 @@ CREATE TABLE users (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     last_login_day DATETIME,
     visit_at DATETIME,
-    point INT DEFAULT 0,
-    coupon_id INT,
-    FOREIGN KEY (coupon_id) REFERENCES coupon(coupon_id)
+    point INT DEFAULT 0
+);
+
+-- クーポンテーブル
+CREATE TABLE coupons (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    menu_id INT,
+    used_at DATETIME,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    experied_at DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (menu_id) REFERENCES menus(id)
 );
 
 -- ポイント履歴テーブル
 CREATE TABLE point_histories (
-    point_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     changed_amount INT NOT NULL,
     reason VARCHAR(255),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 -- ニューステーブル
 CREATE TABLE news (
-    news_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+
