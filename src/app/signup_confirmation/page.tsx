47194b1a96f7b7076fc4437a11b712c5dcb9b89a
辻　悠吾 <c1304282@st.kanazawa-it.ac.jp>
2025-04-26 11:03:40 +0900

    登録確認画面を追加

---
+++ src/app/signup_confirmation/page.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+
+export default function ConfirmRegistration() {
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          fontFamily: 'Arial, sans-serif',
+          textAlign: 'center',
+          backgroundColor: '#fff',
+          height: '80vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '0 20px',
+          }}
+        >
+          <h2 style={{ marginBottom: '30px', fontSize: '24px', fontWeight: 'bold' }}>登録内容確認</h2>
+
+          <div
+            style={{
+              border: '2px solid red',
+              padding: '20px',
+              marginBottom: '40px',
+              width: '100%',
+              maxWidth: '400px',
+              textAlign: 'left',
+            }}
+          >
+            <p><strong>名前:</strong><br />工大太郎</p>
+            <p style={{ marginTop: '20px' }}><strong>メールアドレス:</strong><br />c1234567@st-kanazawa.ac.jp</p>
+            <p style={{ marginTop: '20px' }}><strong>パスワード:</strong><br />********************</p>
+          </div>
+
+          <div style={{ display: 'flex', gap: '20px' }}>
+            <button
+              style={{
+                padding: '10px 20px',
+                backgroundColor: '#4B4B4B',
+                color: '#fff',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                fontWeight: 'bold',
+                width: '100px',
+              }}
+            >
+              戻る
+            </button>
+            <button
+              style={{
+                padding: '10px 20px',
+                backgroundColor: '#4B4B4B',
+                color: '#fff',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                fontWeight: 'bold',
+                width: '100px',
+              }}
+            >
+              登録
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
