e8d74c600d787154df7ebcd128c8aa1a920a7587
MATSUOKAAIRI <aMatsu20191121@gmail.com>
2025-05-06 17:07:29 +0900

    feat:MUIでの実装

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,20 +1,30 @@
-// pages/logout.js
-import React from 'react';
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
-import { Padding } from '@mui/icons-material';
+import styles from '@/styles/Logout.module.css';
+import { Typography, Button, Box } from '@mui/material';
+import Link from "next/link";
+import Image from 'next/image';
+
 export default function LogoutScreen()  {
   return (
-    <>
-    <Header/>
-    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center', backgroundColor: '#fff', height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-
-      <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <img src="hachiko.png" alt="Character" style={{ width: '100px', height: '80px' }}/>
-        <h2>ログアウトしました</h2>
-        <button style={{ padding: '10px 20px', backgroundColor: '#4B4B4B', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>OK</button>
-      </div>
-      
-    </div><Footer/></>
+    <div className={ styles.page }>
+      <Box 
+        component="section"
+        gap="26px"
+        aria-labelledby="logout"
+        className={ styles.box }
+        >
+        <Image src="/hachiko1.png" alt="Character" width="180" height="150"/>
+        <Typography
+          id="logout"
+          variant="h2"
+          fontSize="2.0rem"
+          fontWeight="bold"
+        >
+          ログアウトしました
+        </Typography>
+        <Link href="/" passHref>
+          <Button variant="contained" size="large" color="info">OK</Button>
+        </Link> 
+      </Box> 
+    </div>
   );
 };
