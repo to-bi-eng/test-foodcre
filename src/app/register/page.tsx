2b409dbab61f95fda1c4cf1cea9ebe68c5f17dac
mei <c1419786@st.kanazawa-it.ac.jp>
2025-05-20 18:54:36 +0900

    feat メッセージの追加

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -15,12 +15,15 @@ export default function Login () {
     <div className={styles.title}>登録</div>
     <div className={styles.form}>
         <div className={styles.TextField_name}>
+            <div className={styles.name}>名前：</div>
             <TextField id="outlined-basic" label="name" variant="outlined" sx={{ width: '300px' }}></TextField>
         </div>
         <div className={styles.TextField_mail}>
+            <div className={styles.mail}>メールアドレス：</div>
             <TextField id="outlined-basic" label="mail" variant="outlined" sx={{ width: '300px' }}></TextField>
         </div>
         <div className={styles.TextField_password}>
+            <div className={styles.password}>パスワード：</div>
             <TextField id="outlined-basic" label="password" variant="outlined" sx={{ width: '300px' }} ></TextField>
         </div>
         <div className={styles.Button}style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
