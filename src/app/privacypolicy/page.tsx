50d1bb244afa66c6bf7e1d42a716525cfb1a0a95
tobi <c1319855@st.kanazawa-it.ac.jp>
2025-05-20 16:37:44 +0900

    update:レビューで指摘されたコード部分の修正

--- src/app/privacypolicy/page.tsx
+++ src/app/privacypolicy/page.tsx
@@ -1,28 +1,27 @@
 import styles from '@/styles/privacypolicy.module.css';
-import {Typography, Button, Box} from '@mui/material';
+import { Typography, Button, Box } from '@mui/material';
 // import Link from "next/link";
 
 export default function Privacypolicy() {
     return (
-      <div className={ styles.page }>
+      <div className={ styles.privacypolicy }>
         <Box 
           className={ styles.box }
         >
-          <Typography
-            fontSize="2.0rem"
-            fontWeight="bold"
-          >
-            プライバシー
-          </Typography>
-
-          <Typography
-            fontSize="2.0rem"
-            fontWeight="bold"
-          >
-            ポリシー
-          </Typography>
-
-
+          <div className= { styles.text }>
+            <Typography
+              fontSize="2.0rem"
+              fontWeight="bold"
+            >
+              プライバシー
+            </Typography>
+            <Typography
+              fontSize="2.0rem"
+              fontWeight="bold"
+            >
+              ポリシー
+            </Typography>
+          </div>
           <div className={ styles.button }>
             <Button 
               variant="contained" 
