ee41408bb1453cec649993e1f8a74bbe5bbccf97
mei <c1419786@st.kanazawa-it.ac.jp>
2025-05-27 18:41:54 +0900

    feat ポイント付与画面の完成

--- src/app/add_point/page.tsx
+++ src/app/add_point/page.tsx
@@ -14,11 +14,28 @@ export default function Login () {
     <div className={styles.form}>
         <div className={styles.QR}></div>
         <div className={styles.TextField_name}>
-            <div className={styles.Text1}>店内に設置されているQRコードを会計時に読み取ってください</div>
-        </div>
-        <div className={styles.Text2}>
-            <div className={styles.mail}>※一日に一度のみポイントが加算されます</div>
-        </div>
+            <div className={styles.Text1}>
+                {"店内に設置されている\nQRコードを会計時に\n読み取ってください"
+                .split('\n')
+                .map((line, index) => (
+                    <React.Fragment key={index}>
+                    {line}
+                    <br />
+                    </React.Fragment>
+                ))}
+            </div>
+            </div>
+            <div className={styles.Text2}>
+            {"※一日に一度のみポイントが\n加算されます"
+                .split('\n')
+                .map((line, index) => (
+                <React.Fragment key={index}>
+                    {line}
+                    <br />
+                </React.Fragment>
+                ))}
+            </div>
+
     </div>
 </div>
     );
