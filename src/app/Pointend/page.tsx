a1eca3db4997c51215b0517ec2dafc7bc2d14df1
MinamotoTaiei <c1307861@st.kanazawa-it.ac.jp>
2025-05-27 18:21:16 +0900

    feat: スマホ表示したときの調整とスクロールを可能にしました。

--- src/app/Pointend/page.tsx
+++ src/app/Pointend/page.tsx
@@ -4,17 +4,17 @@ import { Box, Typography, Button } from '@mui/material';
 import styles from '../../styles/Pointend.module.css';
 
 interface PointAppProps {
-  earnedPoints: number;
+  plusPoints: number;
   totalPoints: number;
 }
 
-const PointApp: React.FC<PointAppProps> = ({ earnedPoints, totalPoints }) => {
+const PointApp: React.FC<PointAppProps> = ({ plusPoints, totalPoints }) => {
   return (
     <Box className={styles.container}>
       {/* ポイント付与メッセージ */}
       <Box className={styles.awardedPointsContainer}>
-        <Typography variant="h5" component="h1" className={styles.awardedPointsText}>
-          {earnedPoints}ポイント付与
+        <Typography variant="h4" className={styles.awardedPointsText}>
+          {plusPoints}ポイント付与
           <br />
           されました！
         </Typography>
@@ -22,7 +22,7 @@ const PointApp: React.FC<PointAppProps> = ({ earnedPoints, totalPoints }) => {
 
       {/* 現在の所有ポイント表示 */}
       <Box className={styles.totalPointsContainer}>
-        <Typography variant="body1" className={styles.totalPointsText}>
+        <Typography className={styles.totalPointsText}>
           現在の所有ポイント：
           <br />
           {totalPoints}ポイント
@@ -30,8 +30,8 @@ const PointApp: React.FC<PointAppProps> = ({ earnedPoints, totalPoints }) => {
       </Box>
 
       {/* メッセージ */}
-      <Box className={styles.exchangeMessageContainer}>
-        <Typography variant="body1" className={styles.exchangeMessageText}>
+      <Box className={styles.MessageContainer}>
+        <Typography className={styles.MessageText}>
           getしたポイントを交換して
           <br />
           クーポンを手に入れよう！！
@@ -39,10 +39,12 @@ const PointApp: React.FC<PointAppProps> = ({ earnedPoints, totalPoints }) => {
       </Box>
 
       {/* ポイント交換ボタン */}
+      <br />
       <Box className={styles.buttonContainer}>
         <Button
           variant="contained"
           className={styles.exchangeButton}
+          disableElevation
         >
           ポイントを交換する
         </Button>
