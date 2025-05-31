c9fd61e4d692ece8f61a998d3d5ecc121656de2d
hiroto11111 <c1320790@st.kanazawa-it.ac.jp>
2025-05-31 12:15:05 +0900

    feat:画像説明文追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
+import styles from '@/styles/top.module.css';
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <>
-    </>
+    <div className={styles.page}>
+      <Image src="/hachiko.png" alt="8番らーめんロゴ" width="70" height="70" className={styles.logo} />
+      <Box mb={6}>
+        <Typography
+          variant="h6"
+          fontSize="1.4rem"
+          align="center"
+        >
+          “8番らーめん”に来店して<br />
+          ポイントを貯めて<br />
+          お得なクーポンをゲットしよう！
+        </Typography>
+      </Box>
+      <Typography
+        variant="h6"
+        fontSize='1.3rem'
+        align="center"
+      >
+        このアプリでは、“8番らーめん<br />
+        工大前店”でのみポイントの付与、<br />
+        クーポンの使用ができます
+      </Typography>
+      <Image src="/hachiko.png" alt="キャラクター" width="70" height="70" className={styles.character} />
+    </div>
   );
 }
