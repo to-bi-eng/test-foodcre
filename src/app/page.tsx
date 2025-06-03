f91be5ee48f8ead578b58428f07e22d1dc5e1292
Tomita Rikuto <tomita.rikuto@exc.epson.co.jp>
2025-06-03 20:16:20 +0900

    feat:ユーザーステータスカードの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserCard from '@/components/UserStatusCard/UserStatusCard';
 
 export default function Home() {
   return (
     <>
+      <UserCard name="太郎" points={1234} coupons={5} />
     </>
   );
 }
