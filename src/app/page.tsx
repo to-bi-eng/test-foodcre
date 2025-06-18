515bde843e553625e5130ef65d6271a91e2637e6
hiroto11111 <c1320790@st.kanazawa-it.ac.jp>
2025-06-18 23:33:24 +0900

    fix:フォルダ名変更とapp直下のpage.tsxのuserstatuscardの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import UserCard from '@/components/UserStatusCard/UserStatusCard';
 
 export default function Home() {
   return (
     <>
-      <UserCard name="太郎" points={1234} coupons={5} />
     </>
   );
 }
