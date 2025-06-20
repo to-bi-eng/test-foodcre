9407f8b97216131b14be07e00f4c0e29c1cde69b
MATSUOKAAIRI <aMatsu20191121@gmail.com>
2025-06-20 18:16:25 +0900

    feat:パスワード変更画面の作成

---
+++ src/app/passwordedit/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import Link from "next/link";
+import { useState , useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import styles from '@/styles/passwordedit.module.css';
+
+export default function Passwordedit() {      
+    const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+    const [passwordedit, setpasswordedit] = useState('');
+    const [passwordconfirm, setpasswordconfirm] = useState('');
+    const [isFormValid, setIsFormValid] = useState(false);
+    const router = useRouter();
+    
+    useEffect(() => {
+        const savedData = sessionStorage.getItem('contactData');
+        if (savedData) {
+            const { } = JSON.parse(savedData);
+            setpasswordedit(passwordedit);
+            setpasswordconfirm(passwordconfirm);
+        }
+    }, [passwordconfirm, passwordedit]);
+
+    useEffect(() => {
+        setIsFormValid(!!passwordedit && !!passwordconfirm);
+    }, [ passwordedit, passwordconfirm]);
+    const handleSave = (e: React.FormEvent) => {
+        e.preventDefault(); 
+        if (!isFormValid) {
+            alert("すべてのフィールドを正しく入力してください。");
+        } else {
+            sessionStorage.setItem('contactData', JSON.stringify({ passwordedit, passwordconfirm }));
+            router.push('/contact_confirm');
+        }
+    };
+
+    return (
+    <>
+        <div className={ styles.passwordedit }>
+            <Stack
+                component="form" 
+                gap="26px" 
+                aria-labelledby="contact_one" 
+                className={ styles.stack } 
+            >
+                <Typography
+                    id="contact_heading" 
+                    variant="h1" 
+                    fontSize="2.0rem"
+                    fontWeight="bold"
+                    >
+                    パスワード変更
+                </Typography>
+                <div className={styles.forms}>
+                  <div className={styles.form}>
+                  <Typography
+                    id="edit_heading" 
+                    >
+                    新しいパスワード
+                </Typography>
+                <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined" className={ styles.Typography }>
+                <InputLabel htmlFor="outlined-adornment-password">新しいパスワード</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            className={styles.edit}
+          />
+        </FormControl>
+        </div>
+        <Typography
+                    id="edit_heading" 
+                    >
+                    新しいパスワード（確認）
+                </Typography>
+        <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined" color="primary">
+                <InputLabel htmlFor="outlined-adornment-password" color="primary">新しいパスワード（確認）</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            className="edit"
+          />
+        </FormControl></div>
+                <div className={styles.button}>
+                <Link href="/" passHref>
+                    <Button variant="contained" size="large" disabled={ !isFormValid } onClick={ handleSave } color="info">登録</Button>
+                </Link>
+                </div>
+            </Stack>
+        </div>
+    </>
+);
+}
