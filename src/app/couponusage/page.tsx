2903ca67f7aaa231a23452475941471a06dcfe95
MATSUOKAAIRI <aMatsu20191121@gmail.com>
2025-07-01 18:08:31 +0900

    fix:ボタンの遷移先の変更

--- src/app/couponusage/page.tsx
+++ src/app/couponusage/page.tsx
@@ -36,24 +36,26 @@ export default function CouponUsagePage() {
         aria-labelledby="contact_one"
         className={styles.stack2}
       >
-        <Button
-        variant="contained"
-        className={styles.button}
-        sx={{ width: 150, height: 60, fontSize:"1.5rem" }}
-        color="info"
-      >
-        使用する
-      </Button>
-      <Typography
-        id="contactreview_heading"
-        sx={{
-        variant:"h3",
-        fontSize: "0.75rem",
-        fontWeight:"semi-bold",
-        color: "black",
-        }}
-      >
-        ※「使用する」を押す前に、店員をお呼びください
+        <Link href="/" passHref>
+          <Button
+            variant="contained"
+            className={styles.button}
+            sx={{ width: 150, height: 60, fontSize: "1.5rem" }}
+            color="info"
+          >
+            使用する
+          </Button>
+        </Link>
+        <Typography
+          id="contactreview_heading"
+          sx={{
+            variant: "h3",
+            fontSize: "0.75rem",
+            fontWeight: "semi-bold",
+            color: "black",
+          }}
+        >
+          ※「使用する」を押す前に、店員をお呼びください
         </Typography>
         <Typography
           id="contactreview_heading"
@@ -65,9 +67,9 @@ export default function CouponUsagePage() {
         >
           有効期限：2024/01/01まで
         </Typography>
-        <Link href="/" passHref>
+        <Link href="/coupon" passHref>
           <Button variant="contained" size="large" color="info">
-            ホーム
+            戻る
           </Button>
         </Link>
         <Typography
@@ -77,7 +79,7 @@ export default function CouponUsagePage() {
           fontWeight="semi-bold"
           className={styles.text}
         >
-          ホームに戻ると、もう一度この画面を表示することはできません
+          使用するを押すと、もう一度この画面を表示することはできません
         </Typography>
       </Stack>
     </div>
