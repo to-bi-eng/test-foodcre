032d84867119b36e0fed0d1ed4b0b4866fa791d8
hiroto11111 <c1320790@st.kanazawa-it.ac.jp>
2025-07-16 23:09:11 +0900

    fix:コンフリクト解消

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 export default function AdminRootLayout({
@@ -13,20 +12,4 @@ export default function AdminRootLayout({
       </body>
     </html>
   );
-=======
-import React from 'react';
-
-export default function AdminRootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ja">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
->>>>>>> 81a5fb31d46b32f30c9242bfb40d6b5020d648c5
 }
