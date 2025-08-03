abce4dfabead5b3db08682b70df805ab75c4dc66
SatouYuusei0704 <08kono90su21ba66@gmail.com>
2025-08-03 09:54:54 +0900

    update:@muiに対応するように修正

--- src/app/(main)/qr-error-ui/page.tsx
+++ src/app/(main)/qr-error-ui/page.tsx
@@ -1,26 +1,51 @@
+"use client";
+import { Box, Typography, Button, Stack } from '@mui/material';
 import styles from './error-screen.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function ErrorScreen() {
+  const router = useRouter();
+
   return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.title}>ポイントアプリ</div>
-        <button className={styles['home-button']}>ホーム</button>
-      </header>
+    <Box>
+      <Box component="header" className={styles.header}>
+        <Typography className={styles.title} fontWeight="bold" fontSize={20}>
+          ポイントアプリ
+        </Typography>
+        <Button
+          className={styles['home-button']}
+          variant="contained"
+          color="secondary"
+          onClick={() => router.push('/')}
+        >
+          ホーム
+        </Button>
+      </Box>
 
-      <main className={styles.main}>
-        <h1 className={styles.heading}>ポイント付与に失敗しました</h1>
-        <p className={styles.message}>再度店舗のQRコードを読み取ってください</p>
-        <button className={styles['back-button']}>戻る</button>
-      </main>
+      <Box component="main" className={styles.main}>
+        <Typography className={styles.heading} variant="h4" gutterBottom>
+          ポイント付与に失敗しました
+        </Typography>
+        <Typography className={styles.message} color="text.secondary" gutterBottom>
+          再度店舗のQRコードを読み取ってください
+        </Typography>
+        <Button
+          className={styles['back-button']}
+          variant="contained"
+          color="info"
+          onClick={() => router.back()}
+        >
+          戻る
+        </Button>
+      </Box>
 
-      <footer className={styles.footer}>
-        <div>お問い合わせ</div>
-        <div>8番ラーメン公式HP</div>
-        <div>フードクリエーションHP</div>
-        <div className={styles['sns-icons']}>X　Facebook　Instagram　YouTube</div>
-        <div>8番らーめん公式SNS｜フードクリエーションSNS</div>
-      </footer>
-    </>
+      <Box component="footer" className={styles.footer}>
+        <Typography>お問い合わせ</Typography>
+        <Typography>8番ラーメン公式HP</Typography>
+        <Typography>フードクリエーションHP</Typography>
+        <Typography className={styles['sns-icons']}>X　Facebook　Instagram　YouTube</Typography>
+        <Typography>8番らーめん公式SNS｜フードクリエーションSNS</Typography>
+      </Box>
+      </Box>
   );
 }
