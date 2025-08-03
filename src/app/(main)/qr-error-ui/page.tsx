ec443e6bc63874053f78a5049dd5f351a6d38a53
SatouYuusei0704 <08kono90su21ba66@gmail.com>
2025-08-03 10:16:11 +0900

    update:表示画面を修正

--- src/app/(main)/qr-error-ui/page.tsx
+++ src/app/(main)/qr-error-ui/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Box, Typography, Button, Stack } from '@mui/material';
-import styles from './error-screen.module.css';
+import styles from '@/styles/error-screen.module.css';
 import { useRouter } from 'next/navigation';
 
 export default function ErrorScreen() {
@@ -8,19 +8,6 @@ export default function ErrorScreen() {
 
   return (
     <Box>
-      <Box component="header" className={styles.header}>
-        <Typography className={styles.title} fontWeight="bold" fontSize={20}>
-          ポイントアプリ
-        </Typography>
-        <Button
-          className={styles['home-button']}
-          variant="contained"
-          color="secondary"
-          onClick={() => router.push('/')}
-        >
-          ホーム
-        </Button>
-      </Box>
 
       <Box component="main" className={styles.main}>
         <Typography className={styles.heading} variant="h4" gutterBottom>
@@ -38,14 +25,6 @@ export default function ErrorScreen() {
           戻る
         </Button>
       </Box>
-
-      <Box component="footer" className={styles.footer}>
-        <Typography>お問い合わせ</Typography>
-        <Typography>8番ラーメン公式HP</Typography>
-        <Typography>フードクリエーションHP</Typography>
-        <Typography className={styles['sns-icons']}>X　Facebook　Instagram　YouTube</Typography>
-        <Typography>8番らーめん公式SNS｜フードクリエーションSNS</Typography>
-      </Box>
-      </Box>
+    </Box>
   );
 }
