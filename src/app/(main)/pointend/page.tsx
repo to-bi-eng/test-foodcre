18c2156f6f0198da14a17d13238a4b8abbf5bfc8
tobi <c1319855@st.kanazawa-it.ac.jp>
2025-08-03 13:22:12 +0900

    ポイント終了画面のエラー修正

--- src/app/(main)/pointend/page.tsx
+++ src/app/(main)/pointend/page.tsx
@@ -1,51 +1,20 @@
-'use client'
-import React from 'react';
-import { useSearchParams } from 'next/navigation';
-import { Box, Typography, Button } from '@mui/material';
-import styles from '@/styles/pointEnd.module.css';
+import React, { Suspense } from 'react';
+import PointEndContent from './PointEndContent';
+import { CircularProgress, Box } from '@mui/material';
 
-const PointEndPage = () => {
-  // URLのクエリパラメータを取得するためのフック
-  const searchParams = useSearchParams();
-
-  // URLから 'addPoints' と 'totalPoints' の値を取得
-  // 値が存在しない、または数値でない場合は 0 を使用する
-  const addPoints = Number(searchParams.get('addPoints')) || 0;
-  const totalPoints = Number(searchParams.get('totalPoints')) || 0;
-
+// ローディング中に表示するコンポーネント
+function LoadingFallback() {
   return (
-    <Box className={styles.container}>
-      {/* ポイント付与メッセージ */}
-      <Box className={styles.awardPoint}>
-        <Typography variant="h4">
-          {addPoints}ポイント付与
-          <br />
-          されました！
-        </Typography>
-      </Box>
-
-      {/* 現在の所有ポイント表示 */}
-      <Box className={styles.totalPoint}>
-        <Typography variant="h5">
-          現在の所有ポイント：
-          <br/>
-          {totalPoints}ポイント
-        </Typography>
-      </Box>
-
-      {/* メッセージ */}
-      <Typography className={styles.Message}>
-        getしたポイントを交換して
-        <br />
-        クーポンを手に入れよう！！
-      </Typography>
-
-      {/* ポイント交換ボタン */}
-      <Button className={styles.button} variant="contained">
-        ポイントを交換する
-      </Button>
+    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <CircularProgress />
     </Box>
   );
-};
+}
 
-export default PointEndPage;
+export default function PointEndPage() {
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      <PointEndContent />
+    </Suspense>
+  );
+}
